9698d26a07c2d2eb012b79e156248ccc7c76d317
lh-25 <sprinklecake25@gmail.com>
2024-10-28 08:29:53 -0500

    solved exercise 2

--- exercises.js
+++ exercises.js
@@ -6,8 +6,21 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 // Create a new array where each value is multiplied by 2 and log the new array.
 
 // Your code here
-const multipliedNums = nums.map((num) => {
-  return num * 2
-})
+// const multipliedNums = nums.map((num) => {
+//   return num * 2
+// })
 
-console.log(multipliedNums)
+// console.log(multipliedNums)
+
+
+// EXERCISE 2: Array destructuring
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const [firstTopping, secondTopping] = pizzaToppings
+
+console.log(firstTopping)
+console.log(secondTopping)
+
